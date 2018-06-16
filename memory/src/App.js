0914880e936b232558ad23814cb11c331c33e806
zahra falah <sepidehfallah2@gmail.com>
2018-06-16 03:03:12 -0700

    components/app.js

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Card from "./components/card";
+import Wrapper from "./components/wrapper";
+import images from "./images.json";
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    images
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      // <div className="App">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </header>        
+      </div> */}
+       <Wrapper>  
+           
+       {this.state.images.map(image => (
+        <Card
+        id={image.id}
+        key={image.id}
+        name={image.name}
+        image={image.image}
+        />
+       ))}        
+    </Wrapper>
     );
   }
 }
--- memory/src/components/card/card.js
+++ memory/src/components/card/card.js
@@ -4,8 +4,12 @@ import './card.css';
 
 const card = props => (
   <div className="card" >
-    <img className="card-img-top card-height" src={props.name} alt="" />
+    <img className="card-img" src={props.image} alt={props.name}/>
   </div>
 );
+
+{/* <span onClick={() => props.removeFriend(props.id)} className="remove">
+      𝘅
+</span> */}
 
 export default card;
